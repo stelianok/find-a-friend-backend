7b5e03254f3215d8cc6d589924f8473a83270e0e
Kauã Steliano <stelianok@gmail.com>
2024-04-15 10:48:40 -0300

    Configured project for unit tests

---
+++ src/useCases/useCaseExample.ts
@@ -0,0 +1,11 @@
+/**
+ * imports
+ * 
+ * interface dataRequest {}
+ * 
+ * interface DataResponse {}
+ * export class useCase {
+ *  constructor(private repository: Repository) { }
+    async execute({data}: data){ code }
+  }
+*/
