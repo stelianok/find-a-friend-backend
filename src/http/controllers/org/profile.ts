1acdc8140747621841756c564bddaf6216816a17
Kauã Steliano <stelianok@gmail.com>
2024-04-16 11:50:33 -0300

    Refactored route to use id from token payload

--- src/http/controllers/org/profile.ts
+++ src/http/controllers/org/profile.ts
@@ -1,18 +1,12 @@
-import { OrgAlreadyExistsError } from "@/useCases/error/org-already-exists-error";
 import { makeGetOrgUseCase } from "@/useCases/factories/make-get-org-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { z } from "zod";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  const registerBodySchema = z.object({
-    id: z.string()
-  })
-
-  const { id } = registerBodySchema.parse(request.body);
-
   const getOrgUseCase = makeGetOrgUseCase();
 
-  const { org } = await getOrgUseCase.execute({ id });
+  const { org } = await getOrgUseCase.execute({
+    id: request.org.sub
+  });
 
   return reply.status(200).send({
     org: {
