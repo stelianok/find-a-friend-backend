99d6eba94e864c6726a59a1e011f8f87ecf6d61d
Kauã Steliano <stelianok@gmail.com>
2024-04-16 08:52:31 -0300

    Implemented custom error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import fastify from "fastify";
 import { env } from "./env";
+import { ZodError } from "zod";
 
 export const app = fastify();
 
 app.get("/", function (request, reply) {
   console.log("Les go!");
   return { hello: 'world' }
+})
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // log to an external tool.
+  }
+
+  return reply.status(500).send({ mesage: 'Internal status error' });
 })
