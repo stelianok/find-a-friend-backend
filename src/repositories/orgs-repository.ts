f90748638aee28fb226d3ef936728f8b2cf4ade0
Kauã Steliano <stelianok@gmail.com>
2024-04-16 08:24:04 -0300

    Implemented methods create and findbyEmail

--- src/repositories/orgs-repository.ts
+++ src/repositories/orgs-repository.ts
@@ -2,4 +2,5 @@ import { Prisma, Org } from '@prisma/client';
 
 export interface OrgsRepository {
   create(data: Prisma.OrgCreateInput): Promise<Org>
+  findByEmail(email: string): Promise<Org | null>
 }
--- src/repositories/prisma/prisma-orgs-repository.ts
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -1,8 +1,23 @@
 import { Prisma } from "@prisma/client";
 import { OrgsRepository } from "../orgs-repository";
+import { prisma } from "@/lib/prisma";
 
 export class PrismaOrgsRepository implements OrgsRepository {
-  create(data: Prisma.OrgCreateInput): Promise<{ id: string; name: string[]; email: string; password_hash: string; address: string; state: string; city: string; phone: string; created_at: Date; }> {
-    throw new Error("Method not implemented.");
+  async create(data: Prisma.OrgCreateInput) {
+    const org = await prisma.org.create({
+      data
+    });
+
+    return org;
+  }
+
+  async findByEmail(email: string) {
+    const org = await prisma.org.findUnique({
+      where: {
+        email
+      }
+    })
+
+    return org;
   }
 }
