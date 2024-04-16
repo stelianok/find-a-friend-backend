189fbfc783cfbeb4d4861d3fab28afe5ac6416e8
Kauã Steliano <stelianok@gmail.com>
2024-04-16 10:22:34 -0300

    Created useCase to list organization information

---
+++ src/useCases/factories/make-get-org-use-case.ts
@@ -0,0 +1,9 @@
+import { PrismaOrgsRepository } from "@/repositories/prisma/prisma-orgs-repository";
+import { GetOrgUseCase } from "../get-org";
+
+export function makeGetOrgUseCase() {
+  const orgsRepository = new PrismaOrgsRepository();
+  const useCase = new GetOrgUseCase(orgsRepository);
+
+  return useCase;
+}
