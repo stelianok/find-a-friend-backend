7d74a4b19859fc6b5b28295243a98eb7f4c64c11
Kauã Steliano <stelianok@gmail.com>
2024-04-17 13:34:11 -0300

    Removed auth hook, added new route.

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { registerPet } from "./registerPet";
 import { verifyJWT } from "@/http/middlewares/verify-jwt";
+import { listPets } from "./listPets";
 
 export async function petsRoutes(app: FastifyInstance) {
-  app.addHook('onRequest', verifyJWT);
 
-  app.post('/:orgId/pets', registerPet);
+  app.get('/pets', listPets);
+  app.post('/:orgId/pets', { onRequest: [verifyJWT] }, registerPet);
 
 }
