80e14c59d68078ff2aa542ef61ff5a05ff522486
Kauã Steliano <stelianok@gmail.com>
2024-04-18 08:38:56 -0300

    Added a new filter param in FindManyInCity

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,6 +1,14 @@
 import { Prisma, Pet } from '@prisma/client';
 
+export interface PetFilters {
+  age?: string;
+  energy_level?: string;
+  size?: string;
+  independence_level?: string;
+
+}
+
 export interface PetsRepository {
   create(data: Prisma.PetCreateInput): Promise<Pet>
-  findManyInCity(city: string): Promise<Pet[]>
+  findManyInCity(city: string, filters?: PetFilters): Promise<Pet[]>
 }
